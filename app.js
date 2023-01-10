9d3050895f850cecf9aa2822bf50b115b860d62e
Devin1824 <devster18@gmail.com>
2023-01-10 07:51:59 -0800

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,34 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Devin"
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Reed";
+age = 23;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Ecercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = false;
+
+
+
